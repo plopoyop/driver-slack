e0177f3fcbb7c32955ed0ccb3824b60718854a17
Clément <clement@plopoyop.me>
2020-07-14 16:49:22 +0200

    permit to reply to app_mention event type

--- src/SlackDriver.php
+++ src/SlackDriver.php
@@ -97,7 +97,7 @@ class SlackDriver extends HttpDriver implements VerifiesService
 
         // If the event type isn't 'message' (which should go through BotMan::hears),
         // build a GenericEvent and return it
-        if (isset($eventData['type']) && $eventData['type'] !== 'message') {
+        if (isset($eventData['type']) && $eventData['type'] !== 'message' && $eventData['type'] !== 'app_mention') {
             $eventPayload = json_encode($eventData);
             if ($eventPayload !== false) {
                 $event = new GenericEvent($eventPayload);
